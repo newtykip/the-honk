7b9d4ea1712fceaaa93ec6af200a0505671a2578
newt <newtykins@outlook.com>
2021-06-23 20:02:32 +0100

    CALCUALTORSRSDSDSDS

---
+++ python/calculators/binomial distribution.py
@@ -0,0 +1,15 @@
+import operator as op
+from functools import reduce
+
+def nCr(n, r):
+    r = min(r, n-r)
+    num = reduce(op.mul, range(n, n - r, -1), 1)
+    den = reduce(op.mul, range(1, r + 1), 1)
+    return num / den
+
+def binomial(x, n, p):
+    q = 1 - p
+
+    return nCr(n, x) * (p**x) * (q**(n-x))
+
+print(binomial(5, 100, 0.1))
--- python/calculators/quadratic nth term.py
+++ python/calculators/quadratic nth term.py
@@ -3,18 +3,24 @@ import operator
 def diff(a):
 	return list(map(operator.sub, a[1:], a[:-1]))
 
+def formatNumber(x):
+	if x % 1 == 0:
+		return int(x)
+	else:
+		return x	
+
 sequence = [-0.5,1,4.5,10,17.5]
 row1 = diff(sequence)
 row2 = diff(row1)
 
-a = row2[0] / 2
-b = row1[0] - (3 * a)
-c = sequence[0] - a - b
+a = formatNumber(row2[0] / 2)
+b = formatNumber(row1[0] - (3 * a))
+c = formatNumber(sequence[0] - a - b)
 
 print('''
 a = {0}
-b = {1}
-c = {2}
+b = {2}
+c = {4}
 
-Equation: {0}n^2 + {1}n + {2}
-'''.format(a, b, c))
+Equation: {0}n²{1}{2}n{3}{4}
+'''.format(a, '+' if b >= 0 else '', b, '+' if c >= 0 else '', c))
