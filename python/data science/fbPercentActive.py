6c48925bc5663100ece798257e648a98d190512f
newt! <newtykins@outlook.com>
2021-08-23 14:04:45 +0100

    Update fb percent dataset

--- python/data science/fbPercentActive.py
+++ python/data science/fbPercentActive.py
@@ -19,14 +19,14 @@ data = []
 def population(country):
 	countryCode = pycountry.countries.search_fuzzy(country)[0].alpha_3
 	res = requests.get('https://restcountries.eu/rest/v2/alpha/' + countryCode)
-	return json.loads(res.content)['population']
+	return int(json.loads(res.content)['population'])
 
 for td1, td2 in zip(tds[::2], tds[1::2]):
 	frame = {}
 	frame['country'] = td1.text.strip()
-	frame['active'] = td2.text.strip()
+	frame['active'] = int(td2.text.strip()) * 1000000
 	frame['population'] = population(frame['country'])
-	frame['percentActive'] = (int(frame['active']) * 1000000 / int(frame['population'])) * 100
+	frame['percentActive'] = (frame['active'] / frame['population']) * 100
 	data.append(frame)
 
 # Save the data
