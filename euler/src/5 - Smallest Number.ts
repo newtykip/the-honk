ce0c254abf5a951360aeccf66bc9d32c3590ea40
newt <hi@newty.dev>
2021-10-29 01:21:37 +0100

    (euler) - #5

---
+++ euler/src/5 - Smallest Number.ts
@@ -0,0 +1,19 @@
+/**
+ * Is x disible to n?
+ */
+const isDivisibleTo = (x: number, n: number) => {
+    for (; n > 0; n -= 1) {
+        if (x % n !== 0) return false;
+    }
+
+    return true;
+};
+
+const divisibleTo = (n: number) => {
+    if (n === 1) return 1;
+
+    for (var step = divisibleTo(n - 1), i = step; !isDivisibleTo(i, n); i += step);
+    return i;
+};
+
+console.log(divisibleTo(20));
