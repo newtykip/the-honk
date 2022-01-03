d91a5bf86607b3e096128951c8f3091e2bdedbfb
newtykins <newtykins@outlook.com>
2022-01-03 00:58:31 +0000

    euler(run): use ts-node

--- euler/run.js
+++ euler/run.js
@@ -1,26 +1,32 @@
 const fs = require('fs');
 const chalk = require('chalk');
-const { spawnSync, spawn } = require('child_process');
+const { spawnSync } = require('child_process');
 const perf = require('execution-time')();
 
 // todo: move to ts-node
 
-const run = (file, puzzleName) => {
-    // Calculate the length of the divider for the puzzle
+const generateBanner = text => {
+    // Calculate the length of the divider
     let divider = '--';
 
-    for (let j = 0; j < puzzleName.length; j++) {
+    for (let j = 0; j < text.length; j++) {
         divider += '-';
     }
 
-    // Styling
-    console.log(divider);
-    console.log(chalk.bold(chalk.greenBright(puzzleName)));
-    console.log(divider);
+    return `${divider}
+${chalk.bold(chalk.greenBright(text))}
+${divider}`;
+};
+
+const run = file => {
+    console.log(generateBanner(file.split('.ts')[0]));
 
     // Execute the file
     perf.start();
-    spawnSync('node', [`build/${file}`], { shell: true, stdio: 'inherit' });
+    spawnSync('npx', ['ts-node', `"src/${file}"`], {
+        shell: true,
+        stdio: 'inherit'
+    });
     const results = perf.stop();
 
     // Print time results
@@ -28,49 +34,35 @@ const run = (file, puzzleName) => {
     console.log(chalk.bold(chalk.yellow(`Executed in ${results.words}`)));
 };
 
-new Promise(async resolve => {
-    // Rebuild the files
-    const gulp = spawn('npx', ['gulp-cli', 'build'], { shell: true, stdio: 'pipe' });
+// Get files
+const tsFiles = fs
+    .readdirSync('src')
+    .filter(f => f.endsWith('.ts'))
+    .filter(f => f !== 'utils.ts');
 
-    gulp.stdout.on('data', data => {
-        const message = data.toString();
-        const match = message.match(/Starting '(.*)'/)?.[1];
-        if (match) console.log(chalk.red(`[gulp] Running '${match}' task`));
-    });
+// Extract the puzzle number
+const puzzleNumber = process.argv[2];
 
-    gulp.on('close', () => {
-        console.log();
-        resolve();
-    });
-}).then(() => {
-    // Get files
-    const tsFiles = fs
-        .readdirSync('src')
-        .filter(f => f.endsWith('.ts'))
-        .filter(f => f !== 'utils.ts');
-    const jsFiles = fs
-        .readdirSync('build')
-        .filter(f => f.endsWith('.js'))
-        .filter(f => f !== 'utils.js');
+if (puzzleNumber === 'all' || !puzzleNumber) {
+    tsFiles
+        .sort((a, b) => {
+            a = parseInt(a.split('-')[0]);
+            b = parseInt(b.split('-')[0]);
 
-    try {
-        // Extract the puzzle number
-        const puzzleNumber = process.argv[2];
-        if (isNaN(puzzleNumber)) throw Error();
-
-        // Find the associated puzzle
-        const tsFile = tsFiles.filter(f => f.startsWith(puzzleNumber))[0];
-        const puzzleName = tsFile.split('.ts')[0];
-
-        run(`${puzzleNumber}.js`, puzzleName);
-    } catch (error) {
-        for (let i = 0; i < jsFiles.length; i++) {
-            const file = jsFiles[i];
-            const tsFile = tsFiles[i];
-            const puzzleName = tsFile.split('.ts')[0];
-
-            run(file, puzzleName);
+            return a > b ? 1 : -1;
+        })
+        .forEach(file => {
+            run(file);
             console.log();
-        }
-    }
-});
+        });
+} else if (!isNaN(puzzleNumber)) {
+    // Find the associated puzzle
+    const [file] = tsFiles.filter(f => f.startsWith(puzzleNumber));
+    run(file);
+} else {
+    console.log(
+        chalk.bold(
+            chalk.red('Please ensure that you input the number of the puzzle to run - thank you.')
+        )
+    );
+}
