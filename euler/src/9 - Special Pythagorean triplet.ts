a0d3fba1383713c5d35dea46c819b00b5e8f43b7
newtykins <newtykins@outlook.com>
2022-01-05 20:18:46 +0000

    refactor(euler): add checklist and more thoughts
    -

---
+++ euler/src/9 - Special Pythagorean triplet.ts
@@ -0,0 +1,22 @@
+export {};
+
+const pythagoreanTriplet = (sum: number) => {
+    let a: number,
+        b = 1,
+        c: number;
+
+    for (; b < sum; b++) {
+        a = -((sum * (2 * b) - sum * sum) / (2 * sum - 2 * b));
+
+        if (Math.floor(a) === a) {
+            c = sum - a - b;
+            break;
+        }
+    }
+
+    return { a, b, c };
+};
+
+const triplet = pythagoreanTriplet(1000);
+console.log(triplet);
+console.log(triplet.a * triplet.b * triplet.c);
