4e91fe164f5bf3dbb019fac141e1b310f8546476
newt <hi@newty.dev>
2022-05-08 13:08:11 +0100

    feat: finish arcus functions

--- languages/python/calculators/Trigometric Functions.py
+++ languages/python/calculators/Trigometric Functions.py
@@ -1,10 +1,10 @@
-from cmath import e, sqrt, log
+from cmath import e, pi, sqrt, log
 from _helpers import floatInput
 
 i = sqrt(-1)
 
 compute = lambda numerator, denominator: (numerator / denominator).real
-ln = lambda x: log(x, e)
+computeInverse = lambda x: (-i * log(x, e)).real
 
 sin = lambda x: compute(pow(e, i * x) - pow(e, -i * x), 2 * i)
 cos = lambda x: compute(pow(e, i * x) + pow(e, -i * x), 2)
@@ -13,14 +13,20 @@ csc = lambda x: 1 / sin(x)
 sec = lambda x: 1 / cos(x)
 cot = lambda x: 1 / tan(x)
 
-arcsin = lambda x: (-i * ln((i * x) + sqrt(1 - pow(x, 2)))).real
+arcsin = lambda x: computeInverse((i * x) + sqrt(1 - pow(x, 2)))
+arccos = lambda x: computeInverse(x + sqrt(pow(x, 2) - 1))
+arctan = lambda x: computeInverse((i - x) / (i + x)) / 2
+arccsc = lambda x: computeInverse((pow(x, -1) * i) + sqrt(1 - pow(x, -2)))
+arcsec = lambda x: computeInverse((pow(x, -1)) + sqrt(pow(x, -2) - 1))
+arccot = lambda x: computeInverse((x + i) / (x - i)) / 2
 
-# todo: finish arc functions
-arccos = lambda x: None
-arctan = lambda x: None
-arccsc = lambda x: (-i * (ln((pow(x, -1) * i) + sqrt(1 - pow(x, -2))))).real
-arcsec = lambda x: None
-arccot = lambda x: None
+# todo: hyperbolic functions
+
+# todo: hyperbolic inverse functions
+
+# todo: hyperbolic reciprocal functions
+
+# todo: hyperbolic inverse reciprocal functions
 
 radians = floatInput("Please enter an amount of radians: ")
 
